cfe872e6f1162c455c4e97a32e6afa0e395b9c6c
josemartinezflorimon <josemartinezflorimon@gmail.com>
2024-04-21 01:53:59 -0300

    Update Router component to include home, about, and contact routes

--- src/assets/router/Router.jsx
+++ src/assets/router/Router.jsx
@@ -6,23 +6,24 @@ import { Route, Routes } from 'react-router-dom'
 //header
 import Header from '../components/layout/Header'
 
-//componets
-import Hero from '../components/pages/Hero'
-
 function Router() {
     return (
         <>
-            {/* <Header /> */}
             <Header />
             <Routes>
-                <Route path="/" element={<Hero />} />
-                <Route path="/about" element={
+                <Route path="/" component={
+                    <>
+                        <h1>Home</h1>
+                        <p>Welcome to the home page</p>
+                    </>
+                } />
+                <Route path="/about" component={
                     <>
                         <h1>About</h1>
                         <p>Learn more about us</p>
                     </>
                 } />
-                <Route path="/contact" element={
+                <Route path="/contact" component={
                     <>
                         <h1>Contact</h1>
                         <p>Get in touch with us</p>
