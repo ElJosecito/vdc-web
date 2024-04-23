3dcaa8a52d5aea5d599d194628311e7eb7cdc3ce
josemartinezflorimon <josemartinezflorimon@gmail.com>
2024-04-22 22:18:46 -0300

    add icons, footer, fix router

---
+++ src/assets/components/layout/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+// react icons, fontawesome 6 icons
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa6";
+
+
+function Footer() {
+    return (
+        <footer className='w-full flex flex-col items-center fixed bottom-0'>
+            <div className='max-w-screen-2xl w-full pt-4'>
+                <nav className='flex justify-between items-center w-full h-24 px-8 border-b-2 border-[#011632]'>
+                    <div className='text-2xl font-bold'>Logo</div>
+                    <ul className='flex gap-8 font-semibold text-base'>
+                        <motion.li whileHover={{ scale: 1.3 }}>
+                            <a href='/'>Home</a>
+                        </motion.li>
+                        <motion.li whileHover={{ scale: 1.3 }}>
+                            <a href='/services'>Services</a>
+                        </motion.li>
+                        <motion.li whileHover={{ scale: 1.3 }}>
+                            <a href='/about'>About</a>
+                        </motion.li>
+                        <motion.li whileHover={{ scale: 1.3 }}>
+                            <a href='/contact'>Contact</a>
+                        </motion.li>
+                    </ul>
+                </nav>
+
+                <div className='w-full h-24 flex justify-between items-center '>
+                    <p className='text-[#011632] text-sm'>© 2021 All rights reserved</p>
+                    {/* socials */}
+                    <div className='flex gap-4'>
+                        <div className='flex justify-center items-center h-9 w-9 bg-[#011632] rounded-full'>
+                            <FaFacebookF className='text-white text-base' />
+                        </div>
+                        <div className='flex justify-center items-center h-9 w-9 bg-[#011632] rounded-full'>
+                            <FaInstagram className='text-white text-base' />
+                        </div>
+                        <div className='flex justify-center items-center h-9 w-9 bg-[#011632] rounded-full'>
+                            <FaLinkedinIn className='text-white text-base' />
+                        </div>
+                        <div className='flex justify-center items-center h-9 w-9 bg-[#011632] rounded-full'>
+                            <FaYoutube className='text-white text-base' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='w-full h-16 bg-[#011632] flex justify-center items-center'>
+                <p className='text-white text-sm'>© 2021 All rights reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/assets/router/Router.jsx
+++ src/assets/router/Router.jsx
@@ -3,33 +3,40 @@ import React from 'react'
 //Routes configuration
 import { Route, Routes } from 'react-router-dom'
 
-//header
+//layaout components
 import Header from '../components/layout/Header'
+import Footer from '../components/layout/Footer'
+
+//hero
+import Hero from '../components/pages/Hero'
 
 function Router() {
     return (
         <>
             <Header />
             <Routes>
-                <Route path="/" component={
-                    <>
-                        <h1>Home</h1>
-                        <p>Welcome to the home page</p>
-                    </>
-                } />
-                <Route path="/about" component={
+                <Route path="/" element={<Hero/>} />
+                <Route path="/about" element={
                     <>
                         <h1>About</h1>
                         <p>Learn more about us</p>
                     </>
                 } />
-                <Route path="/contact" component={
+                <Route path="/contact" element={
                     <>
                         <h1>Contact</h1>
                         <p>Get in touch with us</p>
                     </>
                 } />
+                <Route path="/services" element={
+                    <>
+                        <h1>Services</h1>
+                        <p>Discover our services</p>
+                    </>
+                } />
+
             </Routes>
+            <Footer />
         </>
     )
 }
