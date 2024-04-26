b733e9fb534ef87dc0ea447337fce46f7a4e5fe3
josemartinezflorimon <josemartinezflorimon@gmail.com>
2024-04-26 20:00:45 +0000

    header animation on scroll

--- src/assets/components/layout/Header.jsx
+++ src/assets/components/layout/Header.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 
 function Header() {
+
+    const [scroll, setScroll] = useState(false)
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            setScroll(true)
+        } else {
+            setScroll(false)
+        }
+    })
+
+
+
     return (
         <header className='w-full flex justify-center fixed z-50 '>
             <div className='max-w-screen-2xl w-full pt-4 '>
-                <nav className='flex justify-between items-center w-full h-20 bg-[#e6f6feee] px-8 rounded-md backdrop-blur-lg shadow-sm'>
+                <nav className={`flex justify-between items-center w-full h-20 px-8 rounded-md backdrop-blur-lg transition-all duration-500  ${scroll ? 'bg-[#e6f6feee] shadow-sm' : 'bg-transparent'}`}>
                     <div className='text-2xl font-bold'>Logo</div>
                     <ul className='flex gap-8 font-semibold text-base'>
                         <motion.li whileHover={{ scale: 1.5 }} className='text-sm'>
